013af64a13f3bf9bf7f0d4364d2d565823a9c511
Diksi437 <https://github.com/Diksi437/609-41m.git>
2024-09-30 21:35:37 +0500

    Migrations task

---
+++ database/migrations/2024_09_30_161034_create_screenings_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('screenings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('hall_id')->constrained('halls')->onDelete('cascade');
+            $table->foreignId('movie_id')->constrained('movies')->onDelete('cascade');
+            $table->dateTime('start_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('screenings');
+    }
+};
